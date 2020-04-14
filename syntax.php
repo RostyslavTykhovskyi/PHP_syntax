56624ba2b5b78f61599bf39521d81c8fe9e07a12
RostyslavTykhovskyi <62753519+RostyslavTykhovskyi@users.noreply.github.com>
2020-04-14 18:42:07 +0300

    Add files via upload

--- syntax.php
+++ syntax.php
@@ -1,108 +1,114 @@
 <?php
 
 # Конкатенація
-//$a = "100";
-//$b = 23;
-//
-//var_dump($a + $b);
-//echo '<hr>';
-//var_dump($a . $b);
+$a = "100";
+$b = 23;
+
+var_dump($a + $b);
+echo '<br>';
+var_dump($a . $b);
+echo '<hr>';
 
 # foreach, ассоціативний масив
-//$numbers = array("One" => 1, "Two" => 2, "Three" => 3);
-//
-//foreach ($numbers as $key => $number) {
-//    echo "$key $number<br>";
-//}
+$numbers = array("One" => 1, "Two" => 2, "Three" => 3);
+
+foreach ($numbers as $key => $number) {
+    echo "$key $number<br/>";
+}
+echo '<hr>';
 
 # explode(), implode()
-//$text = 'One Two Three Four Five';
-//$arr = explode(' ', $text);
-//
-//foreach ($arr as $x) {
-//    echo "$x<br>";
-//}
-//
-//$text = implode(', ', $arr);
-//
-//echo "<hr>$text";
+$text = 'One Two Three Four Five';
+$arr = explode(' ', $text);
+
+foreach ($arr as $x) {
+    echo "$x<br>";
+}
+
+$text = implode(', ', $arr);
+
+echo "<br>$text<hr>";
 
 # Розіменування
-//$b = 100;
-//
-//echo "$b<hr>";
-//
-//$a = 'b';
-//$$a = 1;
-//
-//echo $b;
+$b = 100;
+
+echo "$b<br>";
+
+$a = 'b';
+$$a = 1;
+
+echo $b . '<hr>';
 
 # Логічні оператори
-//$a = 10;
-//$b = '10';
-//
-//var_dump($a == $b);
-//echo "<br>";
-//var_dump($a === $b);
-//echo "<br>";
-//var_dump($a !== $b);
-//echo "<br>";
-//var_dump($a <=> $b);
+$a = 10;
+$b = '10';
+
+var_dump($a == $b);
+echo '<br>';
+var_dump($a === $b);
+echo '<br>';
+var_dump($a !== $b);
+echo '<br>';
+var_dump($a <=> $b);
+echo '<hr>';
 
 # Приведення типів
-//$a = 10;
-//
-//var_dump($a);
-//echo "<br>";
-//var_dump($a = 10.0);
-//echo "<br>";
-//var_dump("$a");
-//echo "<br>";
-//var_dump($a * 1.1);
-//echo "<br>";
-//var_dump((bool)$a);
+$a = 10;
+
+var_dump($a);
+echo '<br>';
+var_dump($a = 10.0);
+echo '<br>';
+var_dump("$a");
+echo '<br>';
+var_dump($a * 1.1);
+echo '<br>';
+var_dump((bool)$a);
+echo '<hr>';
 
 # Класи, конструктори, наслідування
-//class Base {
-//    function __construct() {
-//        echo "Base class constructor<br>";
-//    }
-//    function __destruct() {
-//        echo "Base class destructor<br>";
-//    }
-//}
-//
-//class Derived extends Base {
-//    function __construct() {
-//        #parent::__construct();
-//        echo "Derived class constructor<br>";
-//    }
-//    function __destruct() {
-//        #parent::__destruct();
-//        echo "Derived class destructor<br>";
-//    }
-//}
-//
-//$obj = new Derived();
+class Base {
+    function __construct() {
+        echo 'Base class constructor<br>';
+    }
+    function __destruct() {
+        echo 'Base class destructor<br>';
+    }
+}
+
+class Derived extends Base {
+    function __construct() {
+        #parent::__construct();
+        echo 'Derived class constructor<br>';
+    }
+    function __destruct() {
+        #parent::__destruct();
+        echo 'Derived class destructor<br>';
+    }
+}
+
+$obj = new Derived();
+unset($obj);
+echo '<hr>';
 
 # Singleton
-//class Singleton {
-//    private static $instance = null;
-//
-//    public static function getInstance() {
-//        if (static::$instance == null) {
-//            static::$instance = new static();
-//        }
-//
-//        return static::$instance;
-//    }
-//
-//    private function __construct() {}
-//    private function __clone() {}
-//    private function __wakeup() {}
-//}
-//
-//$obj1 = Singleton::getInstance();
-//$obj2 = Singleton::getInstance();
-//
-//var_dump($obj1 === $obj2);
+class Singleton {
+    private static $instance = null;
+
+    public static function getInstance() {
+        if (static::$instance == null) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    private function __construct() {}
+    private function __clone() {}
+    private function __wakeup() {}
+}
+
+$obj1 = Singleton::getInstance();
+$obj2 = Singleton::getInstance();
+
+var_dump($obj1 === $obj2);
